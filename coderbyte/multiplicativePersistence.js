868a312164df5e6ad6bd72990029dda0a1ace899
Anna Rogers <2muddybarefeet@gmail.com>
2016-09-12 18:58:26 -0700

    solution

--- coderbyte/multiplicativePersistence.js
+++ coderbyte/multiplicativePersistence.js
@@ -4,3 +4,19 @@ parameter being passed which will always be a positive integer and return its mu
 which is the number of times you must multiply the digits in num until you reach a single digit. 
 For example: if num is 39 then your program should return 3 because 3 * 9 = 27 then 2 * 7 = 14 and finally 
 1 * 4 = 4 and you stop at 4. */
+
+var multiplicativePersistance = function (num) {
+  var splitNum = num.toString().split("");
+  var count = 0;
+  while (splitNum.length > 1) {
+    //reduce and then split up the number to loop though again in next while loop
+    splitNum = splitNum.reduce(function (prev, curr) {
+      console.log(prev,curr);
+      return prev*curr;
+    },1).toString().split("");
+    count++;
+  }
+  return count;
+};
+
+console.log('answer',multiplicativePersistance(4499));
