6e611fa59fc486d19a62b3204a531f95919f0006
Anna Rogers <2muddybarefeet@gmail.com>
2016-09-15 11:04:42 -0700

    latest

--- interviewProblems/nurx.js
+++ interviewProblems/nurx.js
@@ -1,47 +1,31 @@
 // //5 test cases failing
 
-// function maxDifference(a) {
-//     var currentLow = a[0];
-//     var currentHigh = a[0];
-//     var largestDiff = 0;
+function maxDifference(a) {
+    var currentLow = a[0];
+    var currentHigh = a[0];
+    var largestDiff = 0;
 
-//     //maybe include case if only one thing then no difference
+    //maybe include case if only one thing then no difference
 
-//     for (var i=0; i<a.length; i++) {
-//         //if current low more than current then reset high to current and set low to current
-//         if (a[i] < currentLow) {
-//             if (currentHigh - currentLow > largestDiff) largestDiff = currentHigh - currentLow;
-//             currentLow = a[i];
-//             currentHigh = a[i];
-//         } else if (currentHigh < a[i]) {
-//             changesMade = true;
-//             currentHigh = a[i];
-//         }
-//     }
+    for (var i=0; i<a.length; i++) {
+        //if current low more than current then reset high to current and set low to current
+        if (a[i] < currentLow) {
+            if (currentHigh - currentLow > largestDiff) largestDiff = currentHigh - currentLow;
+            currentLow = a[i];
+            currentHigh = a[i];
+        } else if (currentHigh < a[i]) {
+            changesMade = true;
+            currentHigh = a[i];
+        }
+    }
 
-//     //should return 0 on being 0 diff?
-//     return largestDiff ? largestDiff : -1;
-// }
-function maxDiff(a){
-
- var lowWaterMark = a[0];
- var maxDiffSoFar = -1; //assuming this is the desired output if input is something like [5, 4, 3, 2, 1]
- var newLowWaterMark;
-
- a.forEach(function(value, index){
-
-   if(value < lowWaterMark) newLowWaterMark = value;
-
-   if((value - newLowWaterMark) > maxDiffSoFar){
-     maxDiffSoFar = value - newLowWaterMark;
-     lowWaterMark = newLowWaterMark;
-   }
-
- });
-
- return maxDiffSoFar;
-
+    //should return 0 on being 0 diff?
+    return largestDiff ? largestDiff : -1;
 }
 
-console.log(maxDiff([1,3,5]));
-// console.log(maxDifference([7]));
+
+console.log(maxDiff([5]));
+
+
+//----- second attempt
+
