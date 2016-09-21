02dc20d0dec13ca1bdcbea02e641866911ddd6d4
Anna Rogers <2muddybarefeet@gmail.com>
2016-09-21 10:54:13 -0700

    new problem solved

--- coderbyte/factorial.js
+++ coderbyte/factorial.js
@@ -1,1 +1,22 @@
 /*find the factorial of a given number*/
+// RECURSE!
+
+var finalOne = (function () {
+
+  var cache = {};
+
+  return function factorial (n) {
+    if (cache[n]) return cache[n];
+    
+    if (n===0 || n===1){
+      return 1;
+    }
+
+    cache[n] = factorial(n-1)*n;
+    return cache[n];
+  };
+
+})();
+  
+
+console.log(finalOne(4));
