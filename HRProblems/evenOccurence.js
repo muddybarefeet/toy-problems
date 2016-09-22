a072f5c623f88fe0ea297a6d5a51d9628c88c7db
Anna Rogers <2muddybarefeet@gmail.com>
2016-09-22 09:20:08 -0700

    add solution

--- HRProblems/evenOccurence.js
+++ HRProblems/evenOccurence.js
@@ -9,3 +9,25 @@
  * var onlyEven = evenOccurrence([1, 7, 2, 4, 5, 6, 8, 9, 6, 4]);
  * console.log(onlyEven); //  4
 */
+
+var evenOccurrence = function (arr) {
+  var evens = {};
+  var maxCount = 0;
+  arr.forEach(function (element) {
+    if (element%2===0) {
+      if(evens[element]) {
+        evens[element]++;
+        if (evens[element] > maxCount) maxCount = evens[element];
+      } else {
+        evens[element] = 1;
+      }
+    }
+  });
+  if (Object.keys(evens).length === 0) return null;
+  for (var i = 0; i < arr.length; i++) {
+    if (evens[arr[i]] && evens[arr[i]] === maxCount) return arr[i];
+  }
+};
+
+var onlyEven = evenOccurrence([1, 7, 2, 4, 5, 6, 8, 9, 6, 4]);
+console.log(onlyEven); //  4
