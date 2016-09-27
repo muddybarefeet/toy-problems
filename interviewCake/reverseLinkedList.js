ab78f2f79dde3e2ca32dfb9728dc23123bdc7ecc
Anna Rogers <2muddybarefeet@gmail.com>
2016-09-27 15:47:59 -0700

    working progress

--- interviewCake/reverseLinkedList.js
+++ interviewCake/reverseLinkedList.js
@@ -3,3 +3,41 @@
 
 
 
+var reverseLinkedList = function (head) {
+  if (head === null || head.next === null) return head;
+
+  var current = head;
+  var next = head.next;
+  head.next = null;
+
+  var reverse = function (current, previous) {
+    //if the tail
+    if (current.next === null) {
+      current.next = previous;
+      return;
+    }
+    var temp = current.next;
+    current.next = previous;
+    reverse(temp,current);
+  };
+  reverse(next, head);
+
+};
+
+//------OR-------
+
+var reverse2 = function (head) {
+  //could make a copy first and then return the next linked list (not 0(1) space though)
+  var current = head;
+  var prev = null;
+  var next = null;
+
+  while (current) {
+    next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+
+  return prev;
+};
