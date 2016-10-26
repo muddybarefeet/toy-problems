033194c980d2f3f9fd74fcb9143057c9f9794a80
Anna Rogers <2muddybarefeet@gmail.com>
2016-10-26 09:07:09 -0700

    work on solution for singleton but not pass code wars tests

---
+++ codeWars/singletonPattern.js
@@ -0,0 +1,59 @@
+// In software engineering, the singleton pattern is a design pattern that restricts the instantiation of a class to one object. This is useful when exactly one object is needed to coordinate actions across the system.
+
+// Create an Singleton pattern, so there is one object in system.
+
+//---- however these do not work with pseudo classical pattern
+
+var singleton = function(){
+  
+  var that = {};
+
+  return function(){
+    return that;
+  };
+  
+}();
+
+
+var getTheTimeThisStarted = function(name){
+  
+  var called = false;
+  var storedValue;
+
+  function makeStoredValue(){
+    called = true;
+    storedValue = {
+      name: name,
+      time: Date.now()
+    };
+  }
+
+  return function(){
+    if(called){
+      return storedValue;
+    }else{
+      makeStoredValue();
+      return storedValue;
+    }
+  };
+  
+}();
+
+
+var s1 = getTheTimeThisStarted();
+var s2;
+
+setTimeout(function(){
+  s2 = getTheTimeThisStarted();
+  console.log(s1, s2);
+}, 100);
+
+
+
+
+
+
+
+
+
+
